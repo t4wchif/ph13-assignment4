291ac24c608062bc76daa1d5c4fe8be109f4590b
t4wchif <tawchif04@gmail.com>
2026-02-23 00:23:58 +0600

    Add category tabs and define jobs data as an array of objects

---
+++ script.js
@@ -0,0 +1,91 @@
+let jobs = [
+  {
+    id: 1,
+    companyName: 'Meta Platforms',
+    position: 'Software Engineer',
+    location: 'Menlo Park, CA',
+    type: 'Full-time',
+    salary: '$50k - $100k',
+    description:
+      'Develop scalable applications and work on cutting-edge social technologies.',
+    status: 'all',
+  },
+  {
+    id: 2,
+    companyName: 'Netflix',
+    position: 'Data Scientist',
+    location: 'Los Gatos, CA',
+    type: 'Hybrid',
+    salary: '$40k - $80k',
+    description:
+      'Analyze user behavior data to improve movie recommendation algorithms.',
+    status: 'all',
+  },
+  {
+    id: 3,
+    companyName: 'Shopify',
+    position: 'Product Manager',
+    location: 'Remote',
+    type: 'Full-time',
+    salary: '$20k - $60k',
+    description:
+      'Lead e-commerce feature development for millions of global merchants.',
+    status: 'all',
+  },
+  {
+    id: 4,
+    companyName: 'Pathao',
+    position: 'Operations Manager',
+    location: 'Dhaka, BD',
+    type: 'On-site',
+    salary: '৳ 60K - 80K',
+    description:
+      'Manage logistics and operation strategy for ride-sharing services.',
+    status: 'all',
+  },
+  {
+    id: 5,
+    companyName: 'Tesla',
+    position: 'Autopilot Engineer',
+    location: 'Palo Alto, CA',
+    type: 'Full-time',
+    salary: '$60k - $80k',
+    description:
+      'Enhance self-driving technology using advanced computer vision models.',
+    status: 'all',
+  },
+  {
+    id: 6,
+    companyName: 'bKash',
+    position: 'Security Analyst',
+    location: 'Dhaka, BD',
+    type: 'Hybrid',
+    salary: '৳ 70K - 90K',
+    description:
+      'Ensure secure financial transactions for millions of mobile users.',
+    status: 'all',
+  },
+  {
+    id: 7,
+    companyName: 'Amazon',
+    position: 'Cloud Engineer',
+    location: 'Seattle, WA',
+    type: 'Remote',
+    salary: '$30k - $70k',
+    description:
+      'Build and scale infrastructure on AWS for global enterprise clients.',
+    status: 'all',
+  },
+  {
+    id: 8,
+    companyName: '10 Minute School',
+    position: 'Content Creator',
+    location: 'Dhaka, BD',
+    type: 'On-site',
+    salary: '৳ 60k - 80K',
+    description:
+      'Develop engaging educational materials for students across Bangladesh.',
+    status: 'all',
+  },
+];
+let activeTab = 'all';
